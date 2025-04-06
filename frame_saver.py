791f5a3c22b7dbc1439dde9fb343748d6b4c32c4
marcusskinner <marc.skinner0897@gmail.com>
2025-04-06 16:43:28 -0400

    Update

--- frame_saver.py
+++ frame_saver.py
@@ -2,21 +2,30 @@ from FrameGrabber import FrameGrabber
 import cv2
 import time
 
-data_path = "D:/Repos/overwatch_bot/data/"
+# change to local path
+data_path = "D:/Repos/cale76/data/"
 
 if __name__ == "__main__":
     grabber = FrameGrabber()
-
-    count = 4
+    count = 38
+    
     while True:
-        filename = data_path + "frame" + str(count) + ".png"
-        print(filename)
-        frame = grabber.get_frame(resize=None)
-        success = cv2.imwrite(filename, frame)
-        if success:
-            print("saved image")
-        else:
-            print("failed")
+
+        # press l to exit the application
+        if cv2.waitKey(1) == ord('l'):
+            break
+        
+        if time.time() - start_time >= 180:
+        
+            filename = data_path + "frame" + str(count) + ".png"
+            frame = grabber.get_frame(resize=None)
+            success = cv2.imwrite(filename, frame)
             
-        count += 1
-        time.sleep(180)
+            if success:
+                print("saved image")
+            else:
+                print("failed")
+                
+            count += 1
+            start_time = time.time()
+

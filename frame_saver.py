02eb43fe005b926dce1b7b4960079da84496a7d1
marcusskinner <marc.skinner0897@gmail.com>
2025-04-06 17:17:44 -0400

    Update

--- frame_saver.py
+++ frame_saver.py
@@ -2,30 +2,23 @@ from FrameGrabber import FrameGrabber
 import cv2
 import time
 
-# change to local path
 data_path = "D:/Repos/cale76/data/"
 
 if __name__ == "__main__":
     grabber = FrameGrabber()
-    count = 38
+
+    count = 4
     
     while True:
-
-        # press l to exit the application
-        if cv2.waitKey(1) == ord('l'):
-            break
         
-        if time.time() - start_time >= 180:
+        filename = data_path + "frame" + str(count) + ".png"
+        frame = grabber.get_frame(resize=None)
+        success = cv2.imwrite(filename, frame)
         
-            filename = data_path + "frame" + str(count) + ".png"
-            frame = grabber.get_frame(resize=None)
-            success = cv2.imwrite(filename, frame)
+        if success:
+            print("saved image")
+        else:
+            print("failed")
             
-            if success:
-                print("saved image")
-            else:
-                print("failed")
-                
-            count += 1
-            start_time = time.time()
-
+        count += 1
+        time.sleep(180)
